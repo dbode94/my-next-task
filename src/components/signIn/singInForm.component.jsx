4edbdc2d1e231302d33c2df47b9e0924583f57b2
dnis-pc <dbode0694@gmail.com>
2024-01-23 18:22:32 -0500

    final design of Auth & Some SignIn funct added

--- src/components/signIn/singInForm.component.jsx
+++ src/components/signIn/singInForm.component.jsx
@@ -1,15 +1,45 @@
+//TODO: ADD the functionalities of loging in and registering
+
+import { useState } from 'react';
+
 import './singInForm.style.scss'
 
+
+const defaultForm = {
+    email: '',
+    password: ''
+}
+
 const SingInForm = () =>{
+
+    const [formValues, setFormValues] = useState(defaultForm);
+    const {email, password} = formValues;
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        resetValues();
+        console.log(formValues);
+    }
+
+    const resetValues = () =>{
+        setFormValues(defaultForm)
+
+    }
+
+    const changeHandler = (event) =>{
+        const {type , value} = event.target; 
+        setFormValues({...formValues, [type]: value});
+    }
+
     return(
         <div className="singInForm_contaniner">
-            <form className="singIn_form">
+            <form className="singIn_form" onSubmit={handleSubmit}>
                 <h3>Already have an account?</h3>
                 <h5>Sign In:</h5>
-                <input type="email" name="email" placeholder='email' required/>                
-                <input type="password" name="password" placeholder='Password' required/>
+                <input type="email" name="email" placeholder='email' required onChange={changeHandler} value={email}/>                
+                <input type="password" name="password" placeholder='Password' required onChange={changeHandler} value={password}/>
                 <br />
-                <button>Sign In</button>
+                <button type='submit'>Sign In</button>
                 <button>Sign In with Google</button>
             </form>
         </div>
